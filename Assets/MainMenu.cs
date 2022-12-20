e18daf72e1eac7ef6c5ff5a332d1aa9d045181ec
ddrago <dragodiego286@gmail.com>
2022-12-20 09:50:11 +0000

    Initial commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void startExperiment()
+    {
+        Debug.Log("Start");
+    }
+    public void BlueToothConnect()
+    {
+        Debug.Log("Connect");
+    }
+}
