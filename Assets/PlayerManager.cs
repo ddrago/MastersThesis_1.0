40cf6a8817543b0b793e59102390e1600ed260f9
ddrago <dragodiego286@gmail.com>
2022-12-20 17:04:41 +0000

    Tried to implement commands when a user presses a button. Gives error though

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerManager : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Command]
+    public void CmdOnButtonPress()
+    {
+        Debug.Log("Someone Pressed a button!");
+    }
+}
