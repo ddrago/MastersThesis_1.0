63cb480f660db3462a6b92181029a6035ce91f50
ddrago <dragodiego286@gmail.com>
2022-12-23 19:58:02 +0100

    Changed network address to my LAN IP. Furthermore, added a testing text on the UI that allows us to view changes happening on the server when building on both android and windows

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
 
 public class PlayerManager : NetworkBehaviour
 {
     MainMenu ExperimentMenuScriptComponent;
+    Text PseudoConsole;
 
     // Start is called before the first frame update
     void Start()
     {
         ExperimentMenuScriptComponent = GameObject.Find("ExperimentMenu").GetComponent<MainMenu>();
+        PseudoConsole = GameObject.Find("PseudoConsole").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -27,5 +30,7 @@ public class PlayerManager : NetworkBehaviour
     void CmdYoyo()
     {
         Debug.Log("A client Pressed a button!");
+        Debug.Log(PseudoConsole.ToString());
+        PseudoConsole.text = "TADA!";
     }
 }
