a5fdbc6ff6a3ce88f8cb0a5d3e19f515a3ff3f4c
ddrago <dragodiego286@gmail.com>
2022-12-23 17:49:31 +0100

    Changed configuration of how a player sends the command to the server. Now it is sent from the player itself, by continuously checking on update whether a certain boolean becomes true. Slower than before but it does work.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    private bool button1IsPressed = false;
+
     public void startExperiment()
     {
         Debug.Log("Start");
     }
+
     public void BlueToothConnect()
     {
         Debug.Log("Connect");
+    }
+
+    public void PressButton1()
+    {
+        button1IsPressed = true;
+    }
+
+    public bool getButton1IsPressed()
+    {
+        if (button1IsPressed)
+        {
+            button1IsPressed = false;
+            return true;
+        }
+
+        return false;
     }
 }
--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -5,21 +5,27 @@ using Mirror;
 
 public class PlayerManager : NetworkBehaviour
 {
+    MainMenu ExperimentMenuScriptComponent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ExperimentMenuScriptComponent = GameObject.Find("ExperimentMenu").GetComponent<MainMenu>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (ExperimentMenuScriptComponent.getButton1IsPressed())
+        {
+            print("We pressed a button");
+            CmdYoyo();
+        }
     }
 
-    [Command]
-    public void CmdOnButtonPress()
+    [Command(requiresAuthority = false)]
+    void CmdYoyo()
     {
-        Debug.Log("Someone Pressed a button!");
+        Debug.Log("A client Pressed a button!");
     }
 }
