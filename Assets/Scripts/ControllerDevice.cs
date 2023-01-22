68e284d50acccb9836081e414be64f506707eae2
ddrago <dragodiego286@gmail.com>
2023-01-22 15:47:05 +0000

    updated the ControllerDevice class to add the current attribute

--- Assets/Scripts/ControllerDevice.cs
+++ Assets/Scripts/ControllerDevice.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,7 +13,9 @@ using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.Layouts;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.InputSystem.Utilities;
+using UnityEngine.Scripting;
 
+[Preserve]
 [StructLayout(LayoutKind.Explicit)]
 public struct ControllerDeviceState : IInputStateTypeInfo
 {
@@ -22,7 +27,7 @@ public struct ControllerDeviceState : IInputStateTypeInfo
     [FieldOffset(0)] public byte reportId;
     [FieldOffset(1)] public byte something;
 
-    /*[InputControl(name = "dpad", format = "BIT", layout = "Dpad", sizeInBits = 5, defaultState = 8)]*/
+    [InputControl(name = "dpad", format = "BIT", layout = "Dpad", sizeInBits = 5, defaultState = 8)]
     [InputControl(name = "button1", layout = "Button", bit = 0)]
     [InputControl(name = "button2", layout = "Button", bit = 1)]
     [InputControl(name = "button3", layout = "Button", bit = 2)]
@@ -35,21 +40,46 @@ public struct ControllerDeviceState : IInputStateTypeInfo
     [FieldOffset(2)] public int buttons;
 }
 
-[InputControlLayout(stateType = typeof(ControllerDeviceState))]
 #if UNITY_EDITOR
 [InitializeOnLoad]
 #endif
-public class ControllerDevice : InputDevice
+[Preserve]
+[InputControlLayout(stateType = typeof(ControllerDeviceState))]
+public class ControllerDevice : Gamepad
 {
+#if UNITY_EDITOR
+    static ControllerDevice()
+    {
+        Initialize();
+    }
+#endif
+
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Initialize()
+    {
+        InputSystem.RegisterLayout<ControllerDevice>
+        (
+            matches: new InputDeviceMatcher()
+            .WithInterface("HID")
+            .WithManufacturer("Iton Corp.      ")
+        //.WithCapability("inputReportSize", 4)
+        );
+    }
+
     public ButtonControl button1 { get; private set; }
     public ButtonControl button2 { get; private set; }
     public ButtonControl button3 { get; private set; }
     public ButtonControl westButton { get; private set; }  //PREVIOUS TRACK
     public ButtonControl eastButton { get; private set; }  //NEXT TRACK
-    public ButtonControl button6 { get; private set; }  
+    public ButtonControl button6 { get; private set; }
     public ButtonControl midButton { get; private set; }  //PLAY/PAUSE
     public ButtonControl northButton { get; private set; }  //VOLUME UP
     public ButtonControl southButton { get; private set; }  //VOLUME DOWN
+    public bool SelectButtonDown { get; set; }
+    public bool RewindButtonDown { get; set; }
+    public bool PlayPauseButtonDown { get; set; }
+    public bool FastForwardButtonDown { get; set; }
 
     protected override void FinishSetup()
     {
@@ -62,20 +92,43 @@ public class ControllerDevice : InputDevice
         southButton = GetChildControl<ButtonControl>("southButton");
     }
 
-    static ControllerDevice()
-    {
-        InputSystem.RegisterLayout<ControllerDevice>(
-                matches: new InputDeviceMatcher()
-                .WithInterface("HID")
-                .WithManufacturer("Iton Corp.      ")
-                //.WithCapability("inputReportSize", 4)
-        );
+    public static ControllerDevice current { get; private set; }
 
-        if (!InputSystem.devices.Any(x => x is ControllerDevice))
-            InputSystem.AddDevice<ControllerDevice>();
+    public override void MakeCurrent()
+    {
+        base.MakeCurrent();
+        current = this;
     }
 
-    [RuntimeInitializeOnLoadMethod]
-    public static void Initialize()
-    {}
+    protected override void OnRemoved()
+    {
+        base.OnRemoved();
+        if (current == this)
+            current = null;
+    }
+
+    #region Editor
+#if UNITY_EDITOR
+    [MenuItem("Tools/AftvRemote/Create Device")]
+    private static void CreateDevice()
+    {
+        InputSystem.AddDevice<ControllerDevice>();
+    }
+
+    [MenuItem("Tools/AftvRemote/Remove Device")]
+    private static void RemoveDevice()
+    {
+        var customDevice = InputSystem.devices.FirstOrDefault(x => x is ControllerDevice);
+        if (customDevice != null)
+            InputSystem.RemoveDevice(customDevice);
+    }
+#endif
+    #endregion
+
+    // This does not work somehow. 
+    public void OnUpdate()
+    {
+        var state = new ControllerDeviceState();
+        InputSystem.QueueStateEvent(this, state);
+    }
 }
